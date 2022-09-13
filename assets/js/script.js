ced1693f60f41ceb36b44b389c73878cd25ee6c2
Serhii Pavlovych (astoundcommerce) <s.pavlovych@astoundcommerce.com>
2022-09-13 21:18:50 +0300

    version 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,24 @@
 const onInit = () => {
 
-}
+};
 
 const onScroll = () => {
-    let wrapper = document.querySelector('.wrapper');
-    let infocontainer = document.querySelector('.info-container');
-    let header = document.querySelector('header');
+  let wrapper = document.querySelector(".wrapper");
+  let infocontainer = document.querySelector(".info-container");
+  let header = document.querySelector("header");
 
-    window.addEventListener('scroll', () => {
-        if (scrollY > header.clientHeight) {
-            infocontainer.classList.add('show');
-            infocontainer.hidden = false;
-        } else {
-            infocontainer.classList.remove('show');
-            infocontainer.hidden = true;
-        }
-    });
-}
+  window.addEventListener("scroll", () => {
+    if (scrollY > header.clientHeight) {
+      infocontainer.classList.add("show");
+      infocontainer.hidden = false;
+    } else {
+      infocontainer.classList.remove("show");
+      infocontainer.hidden = true;
+    }
+  });
+};
 
 window.onload = () => {
-    onInit();
-    onScroll();
-}
+  onInit();
+  onScroll();
+};
